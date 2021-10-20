fc324179c6dbbd6e5b2ca7ac5e99543c5291f520
Joe Doiron <josephdoiron1234@yahoo.com>
2021-10-20 10:25:13 -0500

    changed token file to a javascript file and added windows to supported operating systems

--- start_server.py
+++ start_server.py
@@ -5,11 +5,13 @@ import webbrowser
 import json
 import subprocess
 import requests
+import re
+import sys
 
 
 HOST = "127.0.0.1"
 PORT = 8888
-
+IP_PATTERN = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
 
 def get_terminal_line(line, line_num):
     if line_num == 0:
@@ -18,17 +20,14 @@ def get_terminal_line(line, line_num):
         line_type = "output"
     return json.dumps({"type": line_type, "msg": line})
 
-def get_ip_from_line(line):
-    line_split = line.strip().split("  ")
-    ip_and_times_list = [val.strip() for val in line_split[1:]]
-    try:
-        index = ip_and_times_list.index(next(filter(lambda x: x != "*", ip_and_times_list)))
-        ip = ip_and_times_list[index]
-    except StopIteration:
-        ip = None
-    return ip
-
-
+def get_ip_from_line(line, line_num):
+    if line_num != 0:
+        ip = IP_PATTERN.search(line)
+        if ip:
+            return ip[0]
+        return None
+    else:
+        return None
 
 async def http_handler(request):
     return web.FileResponse('./index.html')
@@ -45,10 +44,15 @@ async def traceroute(request):
         host = data["host"]
         ip_info_tk = data["token"]
 
-        with subprocess.Popen(["traceroute", "-I", "--max-hop=30", host], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as process:
+        if sys.platform.startswith('linux'):
+            command = ["traceroute", "-I", "--max-hop=30", host]
+        elif sys.platform.startswith('win32'):
+            command = ["tracert", "-h", "30", host]
+
+        with subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as process:
             for i, line in enumerate(process.stdout):
                 await ws.send_str(get_terminal_line(line, i))
-                ip = get_ip_from_line(line)
+                ip = get_ip_from_line(line, i)
                 if ip:
                     ip_geolocation_data = requests.get(f"https://ipinfo.io/{ip}?token={ip_info_tk}").json()
                     geoloc_msg = json.dumps({"type": "geo", "msg": ip_geolocation_data})
